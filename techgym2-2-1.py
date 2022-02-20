f82c72cd3a8b1d0ce1f9127354f26c7600009abd
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2022-02-20 18:27:34 +0900

    Python2-2-1
    実行結果通りに出力できることを確認しました。

--- techgym2-2-1.py
+++ techgym2-2-1.py
@@ -5,11 +5,11 @@ level = 1
 def start_message():
   print('違う漢字の番号(例:A1)を入力してください')
 
-def section_message():
-  print('レベル:' + str(level))
-
 def play():
-  #section_message()
+  def section_message():
+    print('レベル:' + str(level))
+  
+  section_message()
   choice = input('(例:A1)')
   print('デバッグ:choice = ' + choice)
 
